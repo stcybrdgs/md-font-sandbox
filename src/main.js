4c083ae6a8414792808a5a5c31e30ccbd981fe5d
Stacy <stcybrdgs@gmail.com>
2021-01-13 08:00:47 -0600

    adding new files

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Material Icons
+import { MdIcon } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdIcon);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
